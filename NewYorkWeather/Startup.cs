6d8c21c6c692a10e7fbc4bcd1cecd47a486ec25a
Dima Kravchyna <kravchyna@gmail.com>
2017-09-14 15:10:39 +0300

    Added Common UI

---
+++ NewYorkWeather/Startup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using NewYorkWeather.Services;
+using NewYorkWeather.ViewModels;
+
+namespace NewYorkWeather
+{
+    /// <summary>
+    /// Place for the services initialization.
+    /// </summary>
+    public sealed class Startup
+    {
+        private static IServiceProvider _provider;
+
+        public static void AddServices(Action<IServiceCollection> add)
+        {
+            if (add == null)
+                throw new ArgumentNullException(nameof(add));
+
+            IServiceCollection services = new ServiceCollection();
+            add(services);
+            _provider = services.BuildServiceProvider();
+        }
+        public static TValue UseServices<TValue>(Func<IServiceProvider, TValue> use)
+        {
+            if (use == null)
+                throw new ArgumentNullException(nameof(use));
+
+            return use(_provider);
+        }
+    }
+
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddViewModels(this IServiceCollection services)
+        {
+            return services.AddTransient<MainViewModel>();
+        }
+    }
+
+    public sealed class ViewModelLocator
+    {
+        public static MainViewModel MainViewModel =>
+            Startup.UseServices(p => p.GetService<MainViewModel>());
+    }
+}
---
+++ NewYorkWeather/Views/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace NewYorkWeather.UI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = ViewModelLocator.MainViewModel;
+        }
+    }
+}
