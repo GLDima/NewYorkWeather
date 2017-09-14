28a877e494296f620fdd5a9c805f4f6fa7f8d69d
Dima Kravchyna <kravchyna@gmail.com>
2017-09-14 15:14:49 +0300

    Added Dialog Service

---
+++ NewYorkWeather/Services/DialogService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace NewYorkWeather.Services
+{
+    public class DialogService : IDialogService
+    {
+        private readonly CurrentPageService _pageService;
+
+		public Task AlertAsync(string message, string title)
+        {
+            return _pageService.CurrentPage.DisplayAlert(title, message, "Cancel");
+        }
+
+        public DialogService(CurrentPageService pageService)
+        {
+            _pageService = pageService;
+        }
+    }
+}
