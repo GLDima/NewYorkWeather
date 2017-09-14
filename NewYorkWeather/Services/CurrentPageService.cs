8ed969e46805d0cd960a54280d441ab3e2bcf6ae
Dima Kravchyna <kravchyna@gmail.com>
2017-09-14 15:12:46 +0300

    Added CurrentPageService

---
+++ NewYorkWeather/Services/CurrentPageService.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace NewYorkWeather
+{
+    public class CurrentPageService
+    {
+        /// <summary>
+        /// Gets or sets the last loaded page.
+        /// </summary>
+        public Page CurrentPage { get; set; }
+    }
+}
